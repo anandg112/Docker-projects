018ad36864c49ee34925f126730d2b14bb68b4d7
anandg112 <anandg112@gmail.com>
2019-02-03 18:30:14 -0500

    adding a counter app

---
+++ app-counter/visits/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const redis = require("redis");
+
+const app = express();
+
+const PORT = 8081 || process.env.PORT;
+
+const client = redis.createClient();
+client.set('visits', 0); 
+
+app.get('/', (req, res) => {
+    client.get('visits', (err, visits) => {
+        res.send(`Number of visits is ${visits}`);
+        client.set(`visits`, parseInt(visits) + 1);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
